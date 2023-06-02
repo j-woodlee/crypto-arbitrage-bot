de0cb1c74759de79da3f10acfa896484c03eb161
Jake <jake@metalpay.co>
2023-06-01 19:17:32 -0700

    do live check every 5 min

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ const getAveragePurchasePrice = async (exchange, symbol) => {
   let liveCheck = null;
   let live = true;
   while (live) {
-    if (!liveCheck || moment(liveCheck.lastCheck).isBefore(moment().subtract('10', 'minutes'))) {
+    if (!liveCheck || moment(liveCheck.lastCheck).isBefore(moment().subtract('5', 'minutes'))) {
       if (!liveCheck) {
         // eslint-disable-next-line no-await-in-loop
         await new Promise((r) => { setTimeout(r, 5000); }); // wait 5 seconds for sockets to startup
